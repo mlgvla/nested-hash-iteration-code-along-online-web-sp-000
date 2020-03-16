6b945f62b41692f4dcfa48a60a729da856aadf10
Monica Gerard <mlgerardvla@gmail.com>
2020-03-16 00:07:26 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,9 +16,10 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    if person != "Freddy Mercury"
+    if person == "Freddy Mercury"
       contact_details_hash.each do |attribut, data|
-      binding.pry
+        binding.pry
+      end
     end
   end
 end
